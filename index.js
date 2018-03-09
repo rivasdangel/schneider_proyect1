2bb6bc1f02d16ab2e64fed8ac7f7e231066e5ee3
rivasdangel <leonardorivasd@gmail.com>
2018-03-09 15:27:03 -0600

    Update

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var modbus = require('jsmodbus');
-
+var fs = require('fs');
 // create a modbus client
 var client = modbus.client.tcp.complete({
-        'host'              : '192.168.10.100',
+        'host'              : '192.168.100.50',
         'port'              : 502,
         'autoReconnect'     : true,
         'reconnectTimeout'  : 10000,
         'timeout'           : 50000,
         'unitId'            : 0
     });
-
-client.connect();
-
+try{
+  client.connect();
+}catch(error){
+  fs.appendFile('access_log.log', error, function(err){
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+}
 
 client.on('connect', function () {
     client.readHoldingRegisters(0, 10).then(function (resp) {
