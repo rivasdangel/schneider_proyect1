9d3a86d127b17baee8dfb16b9497fc8bcdeedcca
rivasdangel <leonardorivasd@gmail.com>
2018-03-09 17:01:29 -0600

    Update

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var modbus = require('jsmodbus');
 var fs = require('fs');
+
+//Init firestore
+var admin = require('firebase-admin');
+var serviceAccount = require("/credenciales/dbtest-a9f64-firebase-adminsdk-k8qiu-bb52ed8e2a.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+var dataDoor;
+var dataAlarm;
+var flagDoorOpen=false;
+var flagAlarmActive=false;
+
+var idDoor;
+var idAlarm;
 // create a modbus client
 var client = modbus.client.tcp.complete({
         'host'              : '192.168.100.50',
@@ -22,7 +38,46 @@ client.on('connect', function () {
   setInterval(function(){
     client.readHoldingRegisters(0, 4).then(function (resp) {
         //console.log(resp.register);
-        console.log(DataBits(resp.register[0]));
+        //console.log(DataBits(resp.register[0]));
+        if(DataBits(resp.register[0])[15]==1){
+          if(!flagDoorOpen){
+            dataDoor.desc = "Puerta 1 de emergencia: Abierta";
+            dataDoor.ttActive = new Date().getTime();
+            dataDoor.name = "Puerta1";
+            dataDoor.alarmstate = true;
+            db.collection('dbEventos').add(dataDoor).then(function(ref){
+                idDoor = ref.id;
+                console.log('Added document with ID: ', ref.id);
+            });
+            flagDoorOpen=true;
+          }
+        }else{
+          if(flagDoorOpen){
+            db.collection('dbEventos').doc(idDoor).update({ ttInactive: new Date().getTime(), alarmstate:false });
+            console.log("Update Door event.");
+            flagDoorOpen=false;
+          }
+        }
+
+        if(DataBits(resp.register[0])[14]==1){
+          if(!flagAlarmActive){
+            dataAlarm.desc = "Alarma contra incendios: Activa";
+            dataAlarm.ttActive = new Date().getTime();
+            dataAlarm.name = "AlarmaFuego";
+            dataAlarm.alarmstate = true;
+            var addDoc = db.collection('dbEventos').add(dataAlarm).then(function(ref){
+                idAlarm = ref.id;
+                console.log('Added document with ID: ', ref.id);
+            });
+            flagAlarmActive = true;
+          }
+        }else{
+          if(flagAlarmActive){
+            db.collection('dbEventos').doc(idAlarm).update({ ttInactive: new Date().getTime(), alarmstate:false });
+            console.log("Update Alarm event.");
+            flagAlarmActive=false;
+          }
+        }
     }, console.error);
   },1000);
 });
