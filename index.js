7e3e2761ee10d0024e0f1452dc15745baed035e4
rivasdangel <leonardorivasd@gmail.com>
2018-03-13 13:07:38 -0600

    Update try catch

--- index.js
+++ index.js
@@ -33,14 +33,19 @@ var flagAlarmActive=false;
 var idDoor;
 var idAlarm;
 // create a modbus client
-var client = modbus.client.tcp.complete({
-        'host'              : '192.168.100.50',
-        'port'              : 502,
-        'autoReconnect'     : true,
-        'reconnectTimeout'  : 10000,
-        'timeout'           : 50000,
-        'unitId'            : 1
-    });
+try{
+  var client = modbus.client.tcp.complete({
+          'host'              : '192.168.100.50',
+          'port'              : 502,
+          'autoReconnect'     : true,
+          'reconnectTimeout'  : 10000,
+          'timeout'           : 50000,
+          'unitId'            : 1
+      });
+}catch(error){
+  console.log("client Modbus Error.");
+}
+
 try{
   client.connect();
 }catch(error){
@@ -52,47 +57,51 @@ try{
 
 client.on('connect', function () {
   setInterval(function(){
-    client.readHoldingRegisters(0, 4).then(function (resp) {
-        //console.log(resp.register);
-        //console.log(DataBits(resp.register[0]));
-        if(DataBits(resp.register[0])[15]==0){
-          if(!flagDoorOpen){
-            dataDoor.ttActive = new Date().getTime();
-            dataDoor.alarmstate = true;
-            db.collection('dbEventos').add(dataDoor).then(function(ref){
-                idDoor = ref.id;
-                console.log('Added document with ID: ', ref.id);
-            });
-            flagDoorOpen=true;
-            publishMessage(dataDoor);
+    try{
+      client.readHoldingRegisters(0, 4).then(function (resp) {
+          //console.log(resp.register);
+          //console.log(DataBits(resp.register[0]));
+          if(DataBits(resp.register[0])[15]==0){
+            if(!flagDoorOpen){
+              dataDoor.ttActive = new Date().getTime();
+              dataDoor.alarmstate = true;
+              db.collection('dbEventos').add(dataDoor).then(function(ref){
+                  idDoor = ref.id;
+                  console.log('Added document with ID: ', ref.id);
+              });
+              flagDoorOpen=true;
+              publishMessage(dataDoor);
+            }
+          }else{
+            if(flagDoorOpen){
+              db.collection('dbEventos').doc(idDoor).update({ ttInactive: new Date().getTime(), alarmstate:false });
+              console.log("Update Door event.");
+              flagDoorOpen=false;
+            }
           }
-        }else{
-          if(flagDoorOpen){
-            db.collection('dbEventos').doc(idDoor).update({ ttInactive: new Date().getTime(), alarmstate:false });
-            console.log("Update Door event.");
-            flagDoorOpen=false;
-          }
-        }
 
-        if(DataBits(resp.register[0])[14]==1){
-          if(!flagAlarmActive){
-            dataAlarm.ttActive = new Date().getTime();
-            dataAlarm.alarmstate = true;
-            var addDoc = db.collection('dbEventos').add(dataAlarm).then(function(ref){
-                idAlarm = ref.id;
-                console.log('Added document with ID: ', ref.id);
-            });
-            flagAlarmActive = true;
-            publishMessage(dataAlarm);
+          if(DataBits(resp.register[0])[14]==1){
+            if(!flagAlarmActive){
+              dataAlarm.ttActive = new Date().getTime();
+              dataAlarm.alarmstate = true;
+              var addDoc = db.collection('dbEventos').add(dataAlarm).then(function(ref){
+                  idAlarm = ref.id;
+                  console.log('Added document with ID: ', ref.id);
+              });
+              flagAlarmActive = true;
+              publishMessage(dataAlarm);
+            }
+          }else{
+            if(flagAlarmActive){
+              db.collection('dbEventos').doc(idAlarm).update({ ttInactive: new Date().getTime(), alarmstate:false });
+              console.log("Update Alarm event.");
+              flagAlarmActive=false;
+            }
           }
-        }else{
-          if(flagAlarmActive){
-            db.collection('dbEventos').doc(idAlarm).update({ ttInactive: new Date().getTime(), alarmstate:false });
-            console.log("Update Alarm event.");
-            flagAlarmActive=false;
-          }
-        }
-    }, console.error);
+      }, console.error);
+    }catch(error){
+      console.log("Error Read data.");
+    }
   },1000);
 
   setInterval(function(){
@@ -123,21 +132,26 @@ var DataBits=function(num1) {
 };
 
 var publishMessage =  function (mensaje) {
-        var publishConfig = {
-            channel : "burslington_channel",
-            message : {
-                name: mensaje.name,
-                ubicacion: "Acabado",
-                mensaje: mensaje.desc,
-                tipo: "Alarma"
-            },
-            sendByPost: false, // true to send via post
-            storeInHistory: false, //override default storage options
-            meta: {
-                "cool": "meta"
-            } // publish extra meta with the request
-        };
-        pubnub.publish(publishConfig, function(status, response) {
+  try{
+    var publishConfig = {
+        channel : "burslington_channel",
+        message : {
+            name: mensaje.name,
+            ubicacion: "Acabado",
+            mensaje: mensaje.desc,
+            tipo: "Alarma"
+        },
+        sendByPost: false, // true to send via post
+        storeInHistory: false, //override default storage options
+        meta: {
+            "cool": "meta"
+        } // publish extra meta with the request
+    };
+    pubnub.publish(publishConfig, function(status, response) {
 
-        });
+    });
+  }catch(error){
+    console.log("Send message data.");
+  }
+
     };
